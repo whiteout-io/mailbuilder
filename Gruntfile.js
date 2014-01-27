0ec228f9c61963983829735049dc24e8eab06943
Felix Hammerl <mail@felixhammerl.com>
2014-01-27 15:34:24 +0100

    use dot mocha reporter

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
         mochaTest: {
             all: {
                 options: {
-                    reporter: 'Spec'
+                    reporter: 'dot'
                 },
                 src: ['test/*.js']
             }
